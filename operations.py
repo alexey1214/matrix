9d9036e485ed780eb98fa58e27b0ed2db0184ff7
Alexey Prokhorenko <alexey1214@gmail.com>
2021-08-18 18:42:48 +0300

    Rename result into traversal

--- operations.py
+++ operations.py
@@ -1,23 +1,23 @@
 def _traverse(matrix):
-    result = []
+    traversal = []
     size = len(matrix)
 
     for loop in range(size // 2 + size % 2):
 
         for i in range(0 + loop, size - loop):
             elem = matrix[i][0 + loop]
-            result.append(elem)
+            traversal.append(elem)
 
         for j in range((0 + loop) + 1, size - loop):
             elem = matrix[(size - 1) - loop][j]
-            result.append(elem)
+            traversal.append(elem)
 
         for i in range((size - 1) - loop - 1, 0 + loop - 1, -1):
             elem = matrix[i][(size - 1) - loop]
-            result.append(elem)
+            traversal.append(elem)
 
         for j in range((size - 1) - loop - 1, 0 + loop, -1):
             elem = matrix[(0 + loop)][j]
-            result.append(elem)
+            traversal.append(elem)
 
-    return result
+    return traversal
